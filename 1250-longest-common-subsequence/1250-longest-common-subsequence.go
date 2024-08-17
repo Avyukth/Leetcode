f2ac9831cc7c6e10b8507f67f1d32041c552a366
Subhrajit Makur <Avyukth@users.noreply.github.com>
2024-08-17 16:26:49 +0530

    Time: 7 ms (78.34%), Space: 19.1 MB (39.85%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.go
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.go
@@ -1,13 +1,21 @@
 func longestCommonSubsequence(text1 string, text2 string) int {
-    m,n :=len(text1), len(text2)
-    dp := make([][]int, m+1)
-    for i := range dp{
-        dp[i]= make([]int, n+1)
+    m:= len(text1)
+    n:= len(text2)
+    if m==0 || n==0{
+        return 0
     }
-    for i :=1; i<=m; i++{
-        for j :=1; j<=n; j++{
-            if text1[i-1] == text2[j-1]{
-                dp[i][j] = dp[i-1][j-1] +1
+    dp:=make([][]int, m+1)
+    for i := range m+1{
+        dp[i] = make([]int, n+1)
+        dp[i][0]=0
+    }
+    for i :=0 ;i< n+1; i++{
+        dp[0][i] = 0
+    }
+    for i :=1; i< m+1; i++{
+        for j :=1 ; j< n+1; j++{
+            if text1[i-1] ==  text2[j-1]{
+                 dp[i][j] =  dp[i-1][j-1] +1
             }else{
                 dp[i][j] = max(dp[i-1][j] , dp[i][j-1])
             }
