716745f11d720f273da79f190a4eca726e324243
Subhrajit Makur <Avyukth@users.noreply.github.com>
2024-08-19 16:38:28 +0530

    Time: 3 ms (74.49%), Space: 2.3 MB (45.24%) - LeetHub

--- 0792-binary-search/0792-binary-search.rs
+++ 0792-binary-search/0792-binary-search.rs
@@ -1,16 +1,16 @@
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-        let mut left = 0;
-        let mut right = nums.len() as i32 - 1;
-        
-        while left <= right {
-            let mid = left + (right - left) / 2;
-            if nums[mid as usize] == target {
-                return mid;
-            } else if nums[mid as usize] < target {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+        let mut n = nums.len()as i32 -1;
+        let mut start = 0;
+        while start<=n{
+            let mut mid = start + (n- start)/2;
+            if nums[mid as usize] ==target{
+                return mid as i32;
+            }else if nums[mid as usize] <target{ 
+                start= mid+1;
+            }else{
+                n= mid-1;
+
             }
         }
         -1
