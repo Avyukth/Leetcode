1eeec0fefeae88085f49df3041eb5d6622b43d03
Subhrajit Makur <Avyukth@users.noreply.github.com>
2024-08-20 17:38:37 +0530

    Time: 16 ms (23.81%), Space: 7 MB (21.43%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.go
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.go
@@ -1,9 +1,9 @@
 func swimInWater(grid [][]int) int {
     n:= len(grid)
-    left, right := grid[0][0] , n*n-1
-    for left <right {
-        mid := left + (right-left)/2
-        if canReach(grid,n, mid){
+    left , right := grid[0][0] , n*n -1
+    for left <right{
+        mid := left + (right- left)/2
+        if canReach(grid, n, mid ){
             right = mid
         }else{
             left = mid+1
@@ -12,29 +12,32 @@ func swimInWater(grid [][]int) int {
     return left
 }
 
-func canReach(grid [][]int ,n , t int)bool{
+func canReach(grid [][]int, n,mid int )bool{
     visited := make([][]bool, n)
-    for visit := range visited{
-        visited[visit] =make([]bool, n)
+    for i := range n{
+        visited[i] = make([]bool, n)
     }
-    if grid[0][0] > t{
+    if grid[0][0]>mid{
         return false
     }
     que := list.New()
-    que.PushBack([2]int{0, 0})
-    dirs := [4][2]int {{0, -1}, {-1, 0}, {0, 1},{1, 0}}
+    que.PushBack([2]int{0,0})
+    dirs := [4][2]int{{0, -1},{-1, 0}, {1, 0}, {0, 1}}
     for que.Len()>0{
         curr := que.Remove(que.Front()).([2]int)
-        if curr[0] == n-1 && curr[1] == n-1{
+        if curr[0] == n-1 &&  curr[1] == n-1{
             return true
         }
-        for _, dir:= range dirs{
-            nx, ny :=  curr[0] +dir[0], curr[1] +dir[1]
-            if nx>=0 &&  nx<n  && ny>=0 && ny<n && !visited[nx][ny] && grid[nx][ny]<=t{
+        for _, dir := range dirs{
+            nx , ny := curr[0]+ dir[0], curr[1]+ dir[1]
+            if nx >=0 && nx <n && ny >=0 && ny <n && !visited[nx][ny] && grid[nx][ny]<=mid{
                 que.PushBack([2]int{nx, ny})
-                visited[nx][ny] =true
-            }
+                visited[nx][ny] = true
+            }  
         }
     }
     return false
+    
+
 }
+
