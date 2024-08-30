be51b165f5fbbf70855ec283ff47f8eec314e1e4
Subhrajit Makur <Avyukth@users.noreply.github.com>
2024-08-31 00:41:44 +0530

    Time: 4 ms (83.47%), Space: 2.6 MB (84.86%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
@@ -1,25 +1,26 @@
 func longestPalindrome(s string) string {
-    if len(s) ==0{
+    n:= len(s)
+    if n==0 {
         return ""
     }
-    start, end := 0, 0
-    for i:=0; i<len(s); i++{
-        len1, left1, right1 := expand(s,i,i)
-        len2, left2, right2 := expand(s,i,i+1)
-        if len1> end-start{
-            start, end = left1, right1
-        }
-        if len2 > end-start {
-            start, end = left2, right2
+    st , end := 0,0
+    for i:=0 ;i<n ; i++{
+        len1 := expand(s,i,i)
+        len2 := expand(s,i,i+1)
+        lt := max(len1, len2)
+        if lt > end-st{
+            st = i - (lt-1)/2
+            end = i + (lt)/2 
         }
     }
-    return s[start:end+1]
+    return s[st:end+1]
 }
 
-func  expand(s string, left, right int)(int, int, int){
-    for left >=0 && right < len(s) && s[left] ==s[right]{
-        left--
-        right ++
-    }
-    return right - left-1, left+1, right-1
+func expand(s string, i, j int)int{
+    n:= len(s)
+    for i>=0 &&  j < n && s[i] == s[j]{
+        i --
+        j ++
+    } 
+    return j-i-1
 }
